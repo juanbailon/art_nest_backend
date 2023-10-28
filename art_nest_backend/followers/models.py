9847ff08e34cf58e77667b3e8c75bf6de0fcbeec
juanBailon <juanojo2001.5@gmail.com>
2023-10-28 03:08:43 -0500

    creating follow model, serializer. creating test folder. creating urls.py file

--- art_nest_backend/followers/models.py
+++ art_nest_backend/followers/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from users.models import CustomUser
 
 # Create your models here.
+
+class Follow(models.Model):
+
+    follower_user_id = models.ForeignKey(CustomUser, null=False, blank=False, on_delete=models.CASCADE)
+    following_user_id = models.ForeignKey(CustomUser, null=False, blank=False, on_delete=models.CASCADE)
+
