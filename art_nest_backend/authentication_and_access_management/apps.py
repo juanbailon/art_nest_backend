783b905f2703cf7c9781f5f9d03f420eecd4f0ef
juanBailon <juanojo2001.5@gmail.com>
2023-11-14 00:50:22 -0500

    creating authentication_and_access_management app

---
+++ art_nest_backend/authentication_and_access_management/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AuthenticationAndAccessManagementConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'authentication_and_access_management'
