5a7eef675b92cb63f27c68361dd7a4a9c7f21b4c
juanBailon <juanojo2001.5@gmail.com>
2023-11-14 01:21:17 -0500

    adding the authentication_and_access_management app ulrs to the projects urls(auth/)

--- art_nest_backend/art_nest_backend/urls.py
+++ art_nest_backend/art_nest_backend/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('follow/', include('followers.urls')),
+    path('auth/', include('authentication_and_access_management.urls')),
 ]
