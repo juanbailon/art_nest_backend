85cb5f95493f05fdba40e77dfdd6ef880c69e1da
juanBailon <juanojo2001.5@gmail.com>
2023-11-15 16:08:14 -0500

    creating the password reset email OTP related models

--- art_nest_backend/authentication_and_access_management/models.py
+++ art_nest_backend/authentication_and_access_management/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from users.models import CustomUser
 # Create your models here.
+
+class PasswordResetOTP(models.Model):
+
+    OTP = models.CharField(max_length=10, unique=True, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True, null=False)
+    expires_at = models.DateTimeField(null=False, blank=True)
+    user_id =  models.ForeignKey(CustomUser, null=False, blank=False, on_delete=models.CASCADE)
+
+
+class BlacklistedPasswordResetOTP(models.Model):
+
+    OTP = models.OneToOneField(PasswordResetOTP, on_delete=models.CASCADE)
+    blacklisted_at = models.DateTimeField(auto_now_add=True, null=False)
+
+
+class FailedPasswordResetOTPAttemps(models.Model):
+
+    OTP = models.OneToOneField(PasswordResetOTP, on_delete=models.CASCADE)
+    failed_attempts = models.PositiveIntegerField(default=0, null=False, blank=False)
+    last_failed_attempt = models.DateTimeField(auto_now_add=True, null=False)
