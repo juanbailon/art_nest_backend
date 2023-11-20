60e386503716358f25036b964a189d35df425ca9
juanBailon <juanojo2001.5@gmail.com>
2023-11-20 04:00:17 -0500

    creating otp_hanlder.py file, this module handles the stuff realted whit the password reset OTP's (creation, blacklisting, etc)

---
+++ art_nest_backend/authentication_and_access_management/otp_handler.py
@@ -0,0 +1,93 @@
+import secrets
+from typing import Optional
+from datetime import timedelta
+from enum import Enum
+from django.conf import settings
+from django.utils import timezone
+from users.models import CustomUser
+from .models import BlacklistedPasswordResetOTP, FailedPasswordResetOTPAttempts, PasswordResetOTP
+from .exeptions import PasswordResetOTPError
+
+
+class OTPCharacterSet(Enum):
+    NUMBERS = "0123456789"
+    LOWER_CASE_LETTERS = "".join(chr(i) for i in range(97, 123) )
+    CAPITAL_CASE_LETTERS = "".join(chr(i) for i in range(65, 91) )
+    ASCII_CHARS = "".join(chr(i) for i in range(33, 127) )
+
+
+class OTPBlacklistMixin:
+    
+    def check_blacklist(self, otp_code: str) -> None:
+        """
+        Checks if this OTP code is present in the OTP blacklist.  Raises
+        `PasswordResetOTPError` if so.
+        """
+
+        if BlacklistedPasswordResetOTP.objects.filter(OTP__OTP=otp_code).exists():
+            raise PasswordResetOTPError("This OTP code is blacklisted")
+        
+
+    def blacklist(self, OTP: PasswordResetOTP) -> BlacklistedPasswordResetOTP:
+         
+        blacklist_otp_obj, created = BlacklistedPasswordResetOTP.objects.get_or_create(OTP= OTP)
+        return blacklist_otp_obj
+
+
+class FailedOTPAttemptsMixin:
+
+    def record_failed_attempt(self, otp_instance: PasswordResetOTP) -> None:
+        """
+        Records a failed OTP attempt and raises an exception if the maximum
+        allowed attempts are exceeded.
+        """
+        failed_attempts_obj, created = FailedPasswordResetOTPAttempts.objects.get_or_create(
+            OTP=otp_instance
+        )
+
+        if failed_attempts_obj.failed_attempts >= settings.MAX_ALLOWED_ATTEMPS_FOR_OTP_VALIDATION:
+            raise PasswordResetOTPError("Maximum number of failed OTP validation attempts exceeded")
+        
+        if not created:
+            failed_attempts_obj.failed_attempts += 1
+            failed_attempts_obj.save()
+
+         
+            
+class PasswordResetOTPManager(OTPBlacklistMixin, FailedOTPAttemptsMixin):
+
+    def __init__(self,
+                 otp_length:int,
+                 charset:str,
+                 lifetime:timedelta,
+                 ) -> None:
+        
+        self.length = otp_length
+        self.lifetime = lifetime
+        self.charset = charset
+
+
+    @staticmethod        
+    def generate_OTP_code(otp_length:int = 6,
+                 charset:str = OTPCharacterSet.NUMBERS.value
+                ) -> str:
+        
+        return ''.join(secrets.choice(charset) for _ in range(otp_length))
+    
+
+    def record_OTP(self, user: CustomUser) -> PasswordResetOTP:
+        
+        otp = self.generate_OTP_code(otp_length= self.length, charset= self.charset)
+        created_at = timezone.now()
+        expires_at = created_at + settings.PASSWORD_RESET_EMAIL_OTP_LIFETIME
+
+        password_reset_otp_obj = PasswordResetOTP.objects.create(OTP= otp,
+                                        created_at= created_at,
+                                        expires_at= expires_at,
+                                        user = user
+                                        )
+        
+        return password_reset_otp_obj
+
+
+        
