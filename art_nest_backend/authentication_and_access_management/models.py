5a48a737e64e73419fbb28aa34f039c6051ea43f
juanBailon <juanojo2001.5@gmail.com>
2023-11-20 02:23:12 -0500

    fixing typo in model for the failed attemps of  password reset otp

--- art_nest_backend/authentication_and_access_management/models.py
+++ art_nest_backend/authentication_and_access_management/models.py
@@ -16,7 +16,7 @@ class BlacklistedPasswordResetOTP(models.Model):
     blacklisted_at = models.DateTimeField(auto_now_add=True, null=False)
 
 
-class FailedPasswordResetOTPAttemps(models.Model):
+class FailedPasswordResetOTPAttempts(models.Model):
 
     OTP = models.OneToOneField(PasswordResetOTP, on_delete=models.CASCADE)
     failed_attempts = models.PositiveIntegerField(default=0, null=False, blank=False)
