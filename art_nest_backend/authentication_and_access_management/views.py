28d6706f1fe3df7651eb8a57df760deb553b72d7
juanBailon <juanojo2001.5@gmail.com>
2023-11-20 15:10:46 -0500

    modifying send forgot password email view

--- art_nest_backend/authentication_and_access_management/views.py
+++ art_nest_backend/authentication_and_access_management/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from rest_framework import generics, permissions, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from users.models import CustomUser
 from .serializers import ForgotPasswordEmailSerializer
 from .tasks import send_forgot_password_email_task
+from .otp_handler import PasswordResetOTPManager
 
 # Create your views here.
 
@@ -16,8 +18,20 @@ class SendForgotPasswordEmailView(APIView):
         
         if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
 
         email = request.data['email']
-        send_forgot_password_email_task.delay(email, "hola a todas")
+        user = CustomUser.objects.get(email= email)
+
+        otp_manager = PasswordResetOTPManager()
+        password_reset_otp_obj = otp_manager.record_OTP(user= user)
+
+        minutes = otp_manager.lifetime.total_seconds() / 60
+
+        message = f"Your password reset OTP code is: {password_reset_otp_obj.OTP} \n Your code expires in {minutes} minutes"    
+
+        send_forgot_password_email_task.delay(email, message)
 
         return Response({'message': 'forgot password email has been send'}, status=status.HTTP_200_OK)
+    
+
