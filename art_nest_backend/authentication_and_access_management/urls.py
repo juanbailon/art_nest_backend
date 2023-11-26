8fd0cfa4cafe0eb54d41d42da1b021dcaf360c13
juanBailon <juanojo2001.5@gmail.com>
2023-11-26 02:54:26 -0500

    creating the forgot-password/set-new-password endpoint

--- art_nest_backend/authentication_and_access_management/urls.py
+++ art_nest_backend/authentication_and_access_management/urls.py
@@ -16,4 +16,5 @@ urlpatterns = [
     path('api/token/blacklist', TokenBlacklistView.as_view(), name='token_blacklist'),
     path('forgot-password/send/email', views.SendForgotPasswordEmailView.as_view(), name='send_forgot_password_email'),
     path('forgot-password/validate/email/otp', views.ValidateForgotPasswordEmailOTPView.as_view(), name='validates_that_the_email_otp_is_valid'),
+    path('forgot-password/set-new-password', views.SetNewUserPasswordView.as_view(), name='set_a_new_user_password'),
 ]
