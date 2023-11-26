0acef6646c0424d50c712bf3f866ee2eb3dedf54
juanBailon <juanojo2001.5@gmail.com>
2023-11-26 02:56:32 -0500

    creating model TemporaryBlockUser and running migrations for this new model

--- art_nest_backend/authentication_and_access_management/models.py
+++ art_nest_backend/authentication_and_access_management/models.py
@@ -21,3 +21,10 @@ class FailedPasswordResetOTPAttempts(models.Model):
     OTP = models.OneToOneField(PasswordResetOTP, on_delete=models.CASCADE)
     failed_attempts = models.PositiveIntegerField(default=0, null=False, blank=False)
     last_failed_attempt = models.DateTimeField(auto_now=True, null=False)
+
+
+class TemporaryBlockUser(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    block_at = models.DateTimeField(auto_now_add=True)
+    unblock_at = models.DateTimeField(blank=False, null=False)
+    reason = models.TextField(blank=False, null=False)
