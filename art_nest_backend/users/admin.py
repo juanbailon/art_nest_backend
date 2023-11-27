20e8378e6b482ee6d4d480e6aab10a60598aa217
juanBailon <juanojo2001.5@gmail.com>
2023-11-27 02:09:08 -0500

    adding models Avatar and UserProfilePicture to the admin panel

--- art_nest_backend/users/admin.py
+++ art_nest_backend/users/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Avatar, UserProfilePicture
 # Register your models here.
+
+admin.site.register(Avatar)
+admin.site.register(UserProfilePicture)
