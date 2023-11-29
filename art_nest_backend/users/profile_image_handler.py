29c3791a7ee23186f49c11fac1c4856d7096f7a6
juanBailon <juanojo2001.5@gmail.com>
2023-11-29 01:14:37 -0500

    implementing the UserAvatarManager methods

--- art_nest_backend/users/profile_image_handler.py
+++ art_nest_backend/users/profile_image_handler.py
@@ -1,24 +1,49 @@
 from django.db.models.fields.files import ImageFieldFile
+from django.core.exceptions import ObjectDoesNotExist
 from .models import UserAvatar, Avatar, ProfilePicture, CustomUser
+from .exeptions import DefaultAvatarNotFoundError
 
 
 class UserAvatarManager:
     
     @staticmethod
     def set_user_avatar(user: CustomUser, avatar: Avatar) -> UserAvatar:
-        ...
+         
+        user_avatar_obj, created = UserAvatar.objects.get_or_create(user= user, avatar= avatar)
+        return user_avatar_obj
     
     @staticmethod
-    def get_user_avatar(user: CustomUser) -> UserAvatar:
-        ...
+    def get_user_avatar(user: CustomUser) -> UserAvatar | None:
+        try:
+            user_avatar_obj = UserAvatar.objects.get(user= user)
+        except ObjectDoesNotExist:
+            return None
+        
+        return user_avatar_obj
     
     @staticmethod
-    def is_default_avatar(user: CustomUser) -> bool:
-        ...
+    def has_default_avatar(user: CustomUser) -> bool:
+        try:
+            avatar =  Avatar.objects.get(avatar_name= 'default')
+            UserAvatar.objects.get(user= user, avatar= avatar)
+
+        except Avatar.DoesNotExist:
+            raise DefaultAvatarNotFoundError("There is not Avatar object set as the dafault avatar")
+        except UserAvatar.DoesNotExist:
+            return False
+        
+        return True
+        
 
     @staticmethod
     def user_has_avatar(user: CustomUser) -> bool:
-        ...
+        try:
+            UserAvatar.objects.get(user= user)
+        except ObjectDoesNotExist:
+            return False
+        
+        return True
+
 
 
 class ProfilePictureManager:
