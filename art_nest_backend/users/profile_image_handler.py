0e26384704ba19bec173f5eb3a3c2818453720c5
juanBailon <juanojo2001.5@gmail.com>
2023-11-29 00:57:15 -0500

    fixing typos in profile_image_handler.py

--- art_nest_backend/users/profile_image_handler.py
+++ art_nest_backend/users/profile_image_handler.py
@@ -1,4 +1,6 @@
+from django.db.models.fields.files import ImageFieldFile
 from .models import UserAvatar, Avatar, ProfilePicture, CustomUser
+
 
 class UserAvatarManager:
     
@@ -15,14 +17,14 @@ class UserAvatarManager:
         ...
 
     @staticmethod
-    def user_has_avater(user: CustomUser) -> bool:
+    def user_has_avatar(user: CustomUser) -> bool:
         ...
-        
+
 
 class ProfilePictureManager:
     
     @staticmethod
-    def set_user_profile_picture(user: CustomUser, avatar: Avatar) -> UserAvatar:
+    def set_user_profile_picture(user: CustomUser, picture: ImageFieldFile) -> ProfilePicture:
         ...
     
     @staticmethod
