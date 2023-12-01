e45d2561bb1fa04592371790761f34f0d09a63c5
juanBailon <juanojo2001.5@gmail.com>
2023-12-01 15:18:11 -0500

    documenting all stuff in otp_handler.py

--- art_nest_backend/authentication_and_access_management/otp_handler.py
+++ art_nest_backend/authentication_and_access_management/otp_handler.py
@@ -1,5 +1,5 @@
 import secrets
-from datetime import datetime, timedelta
+from datetime import timedelta
 from enum import Enum
 from django.conf import settings
 from django.utils import timezone
@@ -11,6 +11,16 @@ from .exeptions import  BlacklistedOTPError, MaxFailedAttemptsOTPError, ExpiredO
 
 
 class OTPCharacterSet(Enum):
+    """
+    Enumeration representing the different character sets for OTP generation.
+
+    Attributes:
+    - NUMBERS: Numeric characters (0-9).
+    - LOWER_CASE_LETTERS: Lowercase alphabetical characters (a-z).
+    - CAPITAL_CASE_LETTERS: Uppercase alphabetical characters (A-Z).
+    - ASCII_CHARS: All ASCII characters from '!' to '~'.
+    """
+
     NUMBERS = "0123456789"
     LOWER_CASE_LETTERS = "".join(chr(i) for i in range(97, 123) )
     CAPITAL_CASE_LETTERS = "".join(chr(i) for i in range(65, 91) )
@@ -18,6 +28,9 @@ class OTPCharacterSet(Enum):
 
 
 class OTPBlacklistManager:
+    """
+    Manages the blacklisting of password reset OTP codes.
+    """
     
     @staticmethod
     def check_blacklist(otp_instance: PasswordResetOTP) -> None:
@@ -32,6 +45,12 @@ class OTPBlacklistManager:
 
     @staticmethod
     def blacklist(OTP: PasswordResetOTP) -> BlacklistedPasswordResetOTP:
+        """
+        Blacklists the given OTP code.
+        
+        Returns:
+        - BlacklistedPasswordResetOTP: The blacklisted OTP instance.
+        """
          
         blacklist_otp_obj, created = BlacklistedPasswordResetOTP.objects.get_or_create(OTP= OTP)
         return blacklist_otp_obj
@@ -55,12 +74,18 @@ class OTPBlacklistManager:
 
 
 class FailedOTPAttemptsManager:
+    """
+    Manages failed OTP validation attempts.
+    """
 
     @staticmethod
     def record_failed_attempt(otp_instance: PasswordResetOTP) -> FailedPasswordResetOTPAttempts:
         """
         Records a failed OTP attempt and raises an exception if the maximum
         allowed attempts are exceeded.
+
+        Raises:
+        - MaxFailedAttemptsOTPError: If the maximum allowed attempts are exceeded.
         """
 
         max_allowed_failed_attempts = settings.MAX_ALLOWED_ATTEMPS_FOR_OTP_VALIDATION
@@ -89,6 +114,15 @@ class FailedOTPAttemptsManager:
 
     @staticmethod
     def check_max_failed_attempts(otp_instance: PasswordResetOTP) -> None:
+        """
+        Checks if the maximum allowed failed OTP attempts are exceeded.
+
+        Parameters:
+        - otp_instance (PasswordResetOTP): The OTP instance for which the attempts are checked.
+
+        Raises:
+        - MaxFailedAttemptsOTPError: If the maximum failed OTP attempts are exceeded.
+        """
         
         failed_attempts_obj = FailedOTPAttemptsManager.get_failed_otp_attempt(otp_instance= otp_instance)
 
@@ -101,6 +135,12 @@ class FailedOTPAttemptsManager:
 
     @staticmethod
     def get_failed_attempts_count(otp_instance: PasswordResetOTP) -> int:
+        """
+        Gets the count of failed OTP validation attempts.
+
+        Parameters:
+        - otp_instance (PasswordResetOTP): The OTP instance for which the attempts count is retrieved.
+        """
 
         failed_attempts_obj = FailedOTPAttemptsManager.get_failed_otp_attempt(otp_instance= otp_instance)
 
@@ -114,6 +154,9 @@ class FailedOTPAttemptsManager:
     def get_remaining_attempts(otp_instance: PasswordResetOTP) -> int:
         """
         Calculate the remaining attempts for the user to validate their OTP.
+
+        Parameters:
+        - otp_instance (PasswordResetOTP): The OTP instance for which the remaining attempts are calculated.
         """
         num_failed_attempts = FailedOTPAttemptsManager.get_failed_attempts_count(otp_instance= otp_instance)
         
@@ -124,6 +167,16 @@ class FailedOTPAttemptsManager:
 
     @staticmethod
     def get_failed_otp_attempt(otp_instance: PasswordResetOTP) -> FailedPasswordResetOTPAttempts | None:
+        """
+        Gets the failed OTP attempts instance for a given OTP.
+
+        Parameters:
+        - otp_instance (PasswordResetOTP): The OTP instance for which the failed attempts instance is retrieved.
+
+        Returns:
+        - FailedPasswordResetOTPAttempts | None: The failed OTP attempts instance or None if not found.
+        """
+
         try:
             failed_attempts_obj= FailedPasswordResetOTPAttempts.objects.get(
                 OTP= otp_instance
@@ -139,12 +192,23 @@ class FailedOTPAttemptsManager:
          
             
 class PasswordResetOTPManager(OTPBlacklistManager, FailedOTPAttemptsManager):
+    """
+    Manages the generation, validation, and expiration of OTP codes.
+    """
 
     def __init__(self,
                  otp_length:int = settings.OTP_CODE_LENGTH,
                  charset:str = OTPCharacterSet.NUMBERS.value,
                  lifetime:timedelta = settings.PASSWORD_RESET_EMAIL_OTP_LIFETIME,
                  ) -> None:
+        """
+        Initializes the OTP manager with custom parameters.
+
+        Parameters:
+        - otp_length (int): The length of the OTP code.
+        - charset (str): The character set used for generating OTP codes.
+        - lifetime (timedelta): The lifetime of generated OTP codes.
+        """
         
         self.length = otp_length
         self.lifetime = lifetime
@@ -155,11 +219,31 @@ class PasswordResetOTPManager(OTPBlacklistManager, FailedOTPAttemptsManager):
     def generate_OTP_code(otp_length:int = 6,
                  charset:str = OTPCharacterSet.NUMBERS.value
                 ) -> str:
+        """
+        Generates a random OTP code.
+
+        Parameters:
+        - otp_length (int): The length of the OTP code.
+        - charset (str): The character set used for generating OTP codes.
+        """
         
         return ''.join(secrets.choice(charset) for _ in range(otp_length))
     
 
     def _generate_valid_OTP_code(self, user: CustomUser, max_iterations: int) -> str | None:
+        """
+        Generates a valid OTP code for a user.
+
+        Parameters:
+        - user (CustomUser): The user for whom the OTP code is generated.
+        - max_iterations (int): The maximum number of iterations to attempt.
+
+        Returns:
+        - str | None: The generated valid OTP code or None if no valid code is generated.
+
+        Is very unlikely that the reuturn value will be None. The bigger the otp code length
+        the less likely it will be that the returned value will be none
+        """
 
         for _ in range(max_iterations):
             otp_code = self.generate_OTP_code(otp_length= self.length, charset= self.charset)
@@ -170,6 +254,15 @@ class PasswordResetOTPManager(OTPBlacklistManager, FailedOTPAttemptsManager):
 
     
     def record_OTP(self, user: CustomUser) -> PasswordResetOTP:
+        """
+        Records an OTP for a user.
+
+        Parameters:
+        - user (CustomUser): The user for whom the OTP is recorded.
+
+        Returns:
+        - PasswordResetOTP: The recorded OTP instance.
+        """
         
         otp = self._generate_valid_OTP_code(user= user, max_iterations=5)
 
@@ -185,13 +278,10 @@ class PasswordResetOTPManager(OTPBlacklistManager, FailedOTPAttemptsManager):
         return password_reset_otp_obj
     
 
-    def is_valid_OTP_candidate(self,
-                               otp_code: str,
-                            #    created_at: datetime,
-                            #    exprires_at: datetime,
-                               user: CustomUser
-                               ) -> bool:
-        
+    def is_valid_OTP_candidate(self, otp_code: str, user: CustomUser) -> bool:
+        """
+        Checks if an OTP code is a valid candidate for a user.
+        """     
         my_query = PasswordResetOTP.objects.filter(user= user, OTP= otp_code)
 
         return not my_query.exists()
@@ -199,6 +289,12 @@ class PasswordResetOTPManager(OTPBlacklistManager, FailedOTPAttemptsManager):
 
     @staticmethod
     def is_OTP_expired(otp_instance: PasswordResetOTP) -> bool:
+        """
+        Checks if an OTP has expired.
+
+        Parameters:
+        - otp_instance (PasswordResetOTP): The OTP instance to check.
+        """
         current_time = timezone.now()
         expires_at = otp_instance.expires_at
 
@@ -207,6 +303,12 @@ class PasswordResetOTPManager(OTPBlacklistManager, FailedOTPAttemptsManager):
 
     @staticmethod
     def check_expiration_date(otp_instance: PasswordResetOTP) -> None:
+        """
+        Checks the expiration date of an OTP and raises an exception if it is expired.
+
+        Raises:
+        - ExpiredOTPError: If the OTP is expired.
+        """
         flag = PasswordResetOTPManager.is_OTP_expired(otp_instance)
 
         if flag:
@@ -215,7 +317,12 @@ class PasswordResetOTPManager(OTPBlacklistManager, FailedOTPAttemptsManager):
     
     @staticmethod
     def get_all_valid_user_OTPs(user: CustomUser) -> QuerySet[PasswordResetOTP]:
+        """
+        Gets all valid OTPs for a user.
 
+        Returns:
+        - QuerySet[PasswordResetOTP]: A queryset containing all valid OTPs.
+        """
         current_time = timezone.now()
         
         not_expired_otps = PasswordResetOTP.objects.filter(expires_at__gt= current_time, user= user)
@@ -231,12 +338,25 @@ class PasswordResetOTPManager(OTPBlacklistManager, FailedOTPAttemptsManager):
     
     @staticmethod
     def get_all_user_OTPs(user: CustomUser) -> QuerySet[PasswordResetOTP]:
+        """
+        Gets all OTPs for a user.
+
+        Returns:
+        - QuerySet[PasswordResetOTP]: A queryset containing all OTPs.
+        """
 
         return PasswordResetOTP.objects.filter(user= user)
     
 
     @staticmethod
     def get_OTP(user: CustomUser, otp_code: str) -> PasswordResetOTP | None:
+        """
+        Gets an OTP instance for a given user and OTP code.
+
+        Parameters:
+        - user (CustomUser): The user associated with the OTP.
+        - otp_code (str): The OTP code.
+        """
 
         try:
             return PasswordResetOTP.objects.get(user=user, OTP=otp_code)
