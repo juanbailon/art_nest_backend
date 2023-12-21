07998782910b729e8c7f0c1dad54fc1b5cb0f51f
juan <juan.bailon@correounivalle.edu.co>
2023-12-21 04:21:35 -0500

    creating CreateUserPostView

--- art_nest_backend/users_posts/views.py
+++ art_nest_backend/users_posts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import permissions, generics, status
+from rest_framework.response import Response
+from users.permissions import IsProfileOwnerPermission
+from .serializers import UserPostSerializer
 
 # Create your views here.
+
+class CreateUserPostView(generics.CreateAPIView):
+    serializer_class = UserPostSerializer
+    permission_classes = [permissions.IsAuthenticated, IsProfileOwnerPermission]
+
+    def create(self, request, *args, **kwargs):
+        # Access the value of the 'username' path parameter
+        username = kwargs.get('username')
+
+        request.data['user'] = request.user.id
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        data = serializer.data
+        data.pop('user')
+        data['username'] = username
+
+        return Response(data, status=status.HTTP_201_CREATED, headers=headers)
