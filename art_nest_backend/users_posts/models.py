89635e86274d6fa0b91af6ddb4c3233fc606e397
juan <juan.bailon@correounivalle.edu.co>
2023-12-21 02:00:23 -0500

    creating the UserPost model

--- art_nest_backend/users_posts/models.py
+++ art_nest_backend/users_posts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from users.models import CustomUser
 # Create your models here.
+
+class UserPost(models.Model):
+    post_image = models.ImageField(null=False, upload_to='users_posts/')
+    user = models.OneToOneField(CustomUser, on_delete=  models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True, null=False)
